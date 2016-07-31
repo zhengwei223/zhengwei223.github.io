2e052e7494e4644e3c48a8c162bee0d4b6be1b3e
zhengwei <zhengwei@lanqiao.org>
2016-07-31 12:52:26 +0800

    content 响应宽度

--- public/js/base.js
+++ public/js/base.js
@@ -10,9 +10,9 @@ function nav_click(is_show) {
       .removeClass('col-md-13 col-lg-13')  
       .addClass('col-md-8 col-lg-8');   //调整为半屏，如果在手机上因为aside会满屏，所以实际上右侧内容被隐藏了
     /* 调整文字内容格式 */
-    // $('.aside3-content')
-    //   .removeClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2')
-    //   .addClass('col-md-13');  //在显示菜单的情况下填满整行
+    $('.aside3-content')
+      .removeClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2')
+      .addClass('col-md-13');  //在显示菜单的情况下填满整行
 	  
   } else {
     /* 隐藏左侧aside */
@@ -24,9 +24,9 @@ function nav_click(is_show) {
       .removeClass('col-md-8 col-lg-8')
       .addClass('col-md-13 col-lg-13');//右侧最大化
     /* 修改文字排版 */
-    // $('.aside3-content')
-    //   .removeClass('col-md-13')
-    //   .addClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2'); 
+    $('.aside3-content')
+      .removeClass('col-md-13')
+      .addClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2'); 
 	
   }  /*col-md-offset-1 col-lg-offset-2*/
   $("#nav_btn").data('clicked',is_show);
