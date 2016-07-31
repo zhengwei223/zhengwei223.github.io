7a4c4ae686768963bad90a18396c45399c44e061
zhengwei <zhengwei@lanqiao.org>
2016-08-01 00:15:01 +0800

    调整表格样式

--- public/js/base.js
+++ public/js/base.js
@@ -98,6 +98,13 @@ $(document).ready(function() {
     $('#nav_btn').click();
   });
   
+
+    /* For zebra striping */
+    $("table tr:nth-child(odd)").addClass("odd-row");
+    /* For cell text alignment */
+    $("table td:first-child, table th:first-child").addClass("first");
+    /* For removing the last border */
+    $("table td:last-child, table th:last-child").addClass("last");
 });
 
 //生成table of content
