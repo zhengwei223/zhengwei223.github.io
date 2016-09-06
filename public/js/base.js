8e8dde45d8a7b3dbebad3b3f16277035d0f46a61
ZhengWei <zhengwei@lanqiao.org>
2016-09-06 18:41:28 +0800

    修改布局文件，小屏幕可展开侧边栏； 新增一些字体图标。

--- public/js/base.js
+++ public/js/base.js
@@ -16,50 +16,7 @@ function addDuoshuo(){
     } ;
   }
 }
-/* 控制导航按钮动作 */
-function nav_click(is_show) {
-  if (is_show) {
-    /* 显示左侧aside */
-    $('.aside')
-      .addClass('visible-md visible-lg visible-sm visible-xs')  
-      .removeClass('hidden-md hidden-lg hidden-sm hidden-xs')  
-    /* 调整右侧内容 */
-    $('.aside3')
-      .removeClass('col-md-13 col-lg-13')  
-      //调整为半屏，如果在手机上因为aside会满屏，所以实际上右侧内容被隐藏了
-      .addClass('col-md-8 col-lg-8');   
-    /* 调整文字内容格式 */
-    $('.aside3-content')
-      .removeClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2')
-      .addClass('col-md-13');  //在显示菜单的情况下填满整行
-	  
-  } else {
-    /* 隐藏左侧aside */
-    $('.aside')
-      .removeClass('visible-md visible-lg visible-sm visible-xs')
-      .addClass('hidden-md hidden-lg hidden-sm hidden-xs');//隐藏左侧
-    /* 右侧内容最大化 */
-    $('.aside3')
-      .removeClass('col-md-8 col-lg-8')
-      .addClass('col-md-13 col-lg-13');//右侧最大化
-    /* 修改文字排版 */
-    $('.aside3-content')
-      .removeClass('col-md-13')
-      .addClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2'); 
-	
-  }  /*col-md-offset-1 col-lg-offset-2*/
-  $("#nav_btn").data('clicked',is_show);
-}
-/* 控制文章章节列表按钮 */
-function content_click(is_show){
-  if (is_show) {
-    $('#content_table').show();
-    $('#content_btn i').removeClass('fa-plus').addClass('fa-minus');
-  } else {
-    $('#content_table').hide();
-    $('#content_btn i').removeClass('fa-minus').addClass('fa-plus');
-  }
-}
+
 //生成table of content
 function contentEffects(){
   var $h1List ;
@@ -89,6 +46,7 @@ function contentEffects(){
         $li.find('a').click(function(event){
           event.preventDefault();
           $('body,html').animate({scrollTop:$('#'+id).offset().top-60}, 1000);
+          $('#sidebar').collapse('toggle')
         });
       })(id);
       $nav.append($li);
@@ -112,6 +70,7 @@ function contentEffects(){
             $subLi.click(function(event){
               event.preventDefault();
               $('body,html').animate({scrollTop:$('#'+h2id).offset().top-60}, 1000);
+              $('#sidebar').collapse('toggle')
             });
           })(h2id);
           $subUl.append($subLi);
@@ -228,18 +187,21 @@ function otherConfig(){
   $('[data-toggle="tooltip"]').tooltip();
 }
 function addListener(){
-  //展示本文目录
-  $("#content_btn").on('click', function(){
-    isClicked = $(this).data('clicked');
-    content_click(!isClicked);
-    $(this).data('clicked',!isClicked);
-  });
+
   //回到顶部
   $('.scrolltop-btn').on("click", function() {
     //65=h1的上外边距（25）+导航条的高度（40）
     $('body,html').animate({scrollTop:$('.doc-title').offset().top-65}, 500);
+    $('#sidebar').collapse('toggle') //隐藏目录
   });
-
+//侧边栏隐藏时，按钮样式为展开
+  $('#sidebar').on('hidden.bs.collapse', function () {
+    $('.sidebar-toggle>i').removeClass('icon-zhankai').addClass('icon-shouqi');
+  })
+  //侧边栏显示时，按钮样式为收起
+  $('#sidebar').on('shown.bs.collapse', function () {
+    $('.sidebar-toggle>i').removeClass('icon-shouqi').addClass('icon-zhankai');
+  })
 }
 
 function addTableStyle(){
@@ -255,7 +217,6 @@ function addScrollspyAndAffix(){
   // Scrollspy
   var $window = $(window);
   var $body   = $(document.body);
-  var $sideBar = $('.doc-sidebar');
   var off = $('.navbar').height()-1+25;
   $body.scrollspy({
     target: '.doc-side-container',
@@ -263,25 +224,29 @@ function addScrollspyAndAffix(){
   });
   
   $body.scrollspy('refresh');
+  var $sidebarToggle=$('.sidebar-toggle');
+  //仅在大屏幕上affix
+  if(!$sidebarToggle||$sidebarToggle.css('display')==='none'){
+    var $sideBar = $('.doc-sidebar');
+  // 指定开关阈值，滚动到top时，将元素固定在顶部，滚动到bottom时，停止固定
+    $sideBar.affix({
+      offset:{
+        top : function () {
+          var offsetTop      = $sideBar.offset().top
+          var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+          var navOuterHeight = $('.navbar').height()
+          var topValue = offsetTop - navOuterHeight - sideBarMargin;
 
-// 指定开关阈值，滚动到top时，将元素固定在顶部，滚动到bottom时，停止固定
-  $sideBar.affix({
-    offset:{
-      top : function () {
-        var offsetTop      = $sideBar.offset().top
-        var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-        var navOuterHeight = $('.navbar').height()
-        var topValue = offsetTop - navOuterHeight - sideBarMargin;
-
-        return (this.top = topValue);
+          return (this.top = topValue);
+        }
+        ,
+        bottom: function () {
+          var heightOfFooter = $('footer').outerHeight(true);
+          return (this.bottom = heightOfFooter);
+        }
       }
-      ,
-      bottom: function () {
-        var heightOfFooter = $('footer').outerHeight(true);
-        return (this.bottom = heightOfFooter);
-      }
-    }
-  });
+    });
+  }
 }
 
 function iFrameHeight(obj){
